2042a52de92b8448cbee4f7dba3794943e98362c
Nicolas Castano <castanonicolas.nc@gmail.com>
2018-06-01 18:30:08 +0200

    Ajout de la class action

--- src/ricm3/game/automaton/Action.java
+++ src/ricm3/game/automaton/Action.java
@@ -1,5 +1,24 @@
 package ricm3.game.automaton;
 
 public class Action {
+	TypeAction m_action;
+	Direction m_direction;
 
+	//Constructeur apr defaut
+	public Action(TypeAction action, Direction d){
+		m_action = action;
+		m_direction = d;
+	}
+	
+	//Contructeur par Copie
+	public Action( Action a){
+		this(a.getaction, a.getDirection);
+	}
+	
+	public TypeAction getaction() {
+		return m_action;
+	}
+	public Direction getDirection() {
+		return m_direction;
+	}
 }
---
+++ src/ricm3/game/automaton/TypeAction.java
@@ -0,0 +1,5 @@
+package ricm3.game.automaton;
+
+public enum TypeAction {
+	MOVE, TURN;
+}
