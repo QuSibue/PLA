cf5fd7e76cce27fade6fb4c9eca94192651e2a76
raphael <raphael.audin@etud.univ-grenoble-alpes.fr>
2018-06-01 11:18:13 +0200

    implémentation entity

--- src/ricm3/game/entity/Entity.java
+++ src/ricm3/game/entity/Entity.java
@@ -1,5 +1,27 @@
 package ricm3.game.entity;
 
+import java.awt.image.BufferedImage;
+
 public abstract class Entity {
+	
+	int coordinateX;
+	int coordinateY;
+	boolean moveable;
+	boolean pickable;
+	boolean killable;
+	boolean lethal;
+	BufferedImage[] sprites;	
+	
+	public Entity(int x, int y, boolean move, boolean pick, boolean kill, boolean leth, BufferedImage[] spri) {
+		coordinateX=x;
+		coordinateY=y;
+		moveable=move;
+		pickable=pick;
+		killable=kill;
+		lethal=leth;
+		sprites=spri;
+		
+	}
+	
 
 }
