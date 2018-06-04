b28b8ce4e556fd2ff307a1486199e73f8e1f3dcc
Julien Sorbier <julien.sorbier@etu.univ-grenoble-alpes.fr>
2018-06-04 17:01:36 +0200

    laser class ver2

--- src/ricm3/game/entity/Laser.java
+++ src/ricm3/game/entity/Laser.java
@@ -4,148 +4,303 @@ public class Laser extends Being {
 
 	int lifespan;
 	int damage;
-	int type;
 
-	Laser(int lifespan, int damage, int type) {
+	// constructor
+	Laser(int lifespan, int damage) {
 		this.lifespan = lifespan;
 		this.damage = damage;
-		this.type = type;
+
 	}
 
-	// the laser splits in two
-	public void pop(Laser l, Map map) {
+	// getters , setters
+	int getLife(Laser l) {
+		return l.lifespan;
+	}
 
-		if (l.lifespan == 1) {
+	boolean setLife(Laser l, int lifespan) {
+		l.lifespan = lifespan;
+		return true;
+	}
+
+	int getDam(Laser l) {
+		return l.damage;
+	}
+
+	boolean setDam(Laser l, int damage) {
+		l.damage = damage;
+		return true;
+	}
+
+	// POP : the laser splits in two
+	public void pop(Laser l) {
+
+		int life = getLife(l);
+		int dam = getDam(l);
+
+		if (life == 1) {
 			return;
 		}
 
 		else {
-			l.lifespan = l.lifespan / 2;
-			splitter(l,map);
-			Laser l2 = new Laser(l.lifespan, l.damage, l.type);
-			splitter(l2,map);
+
+			life = life / 2;
+
+			setLife(l, life);
+			setDam(l, dam);
+			splitter(l);
+			/*
+			 * Laser l2 = new Laser(life, dam); splitter(l2);
+			 */
 		}
 
 	}
 
-	// auxiliary of pop, searches for empty tiles around pop location, calls Wizz if none is available
+	// auxiliary of pop, searches for empty tiles around pop location, calls Wizz if
+	// none is available
+	// CHECK NAMES OF EXTERNAL METHODS : GETORIENTATION,
 
-	public void splitter(Laser l, Map map) {
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
+	public void splitter(Laser l) {
 
-		if ((map.getEntity(cx+1, cy)) == null) {
-			l.coordinateX = cx + 1;
-		} else if (map.getEntity(cx, cy+1) == null) {
-			l.coordinateY = cy + 1;
-		} else if (map.getEntity(cx+1, cy+1) == null) {
-			l.coordinateY = cy + 1;
-			l.coordinateY = cy + 1;
-		} else if (map.getEntity(cx, cy-1) == null) {
-			l.coordinateY = cy - 1;
-		} else if (map.getEntity(cx-1, cy) == null) {
-			l.coordinateX = cx - 1;
-		} else if (map.getEntity(cx-1, cy-1) == null) {
-			l.coordinateY = cy - 1;
-			l.coordinateY = cy - 1;
-		} else if (map.getEntity(cx+1, cy-1) == null) {
-			l.coordinateY = cy - 1;
-			l.coordinateY = cy + 1;
-		} else if (map.getEntity(cx-1, cy+1) == null) {
-			l.coordinateY = cy + 1;
-			l.coordinateY = cy - 1;
-		} else {
-			Wizz(l);
+		int cx = GetX(l);
+		int cy = GetY(l);
+		Orientation o = getOrientation(l);
+		int life = getLife(l);
+		int dam = getDam(l);
+		int dx1, dy1, dx2, dy2;
+		Laser l_copie = l; // ??? Dunno if it's still l or a copy, bug expected
+
+		// we get coordinates of the tile in front of the laser
+		int dx, dy;
+		positionRelative(cx, cy, dx, dy, up, o);
+
+		if (getEntity(dx, dy) == null) { // tile in front is free
+
+			// we get the coordinate of the tiles up left and up right relatively to the
+			// laser's position
+			positionRelative(dx, dy, dx1, dy1, right, o);
+			positionRelative(dx, dy, dx2, dy2, left, o);
+
+			if (getEntity(dx1, dy1) == null) { // one half "moved" diagonally
+				setEntity(dx1, dy1, l);
+				setOrientation(l, right);
+			} else { // laser causes explosion in front of itself
+				setX(dx, dy, l);
+				wizz(l);
+			}
+			if (getEntity(dx2, dy2) == null) { // one half "moved" diagonally
+				setEntity(dx2, dy2, l_copie);
+				setOrientation(l_copie, left);
+			} else { // laser causes explosion in front of itself
+				setX(dx, dy, l_copie);
+				wizz(l_copie);
+			}
+		}
+
+		else { // getEntity(dx,dy) != null
+
+			// we get the coordinates of the tiles left and right of the laser
+			positionRelative(cx, cy, dx1, dy1, right, o);
+			positionRelative(cx, cy, dx2, dy2, left, o);
+
+			if (getEntity(dx1, dy1) == null) { // one half "moved" on the right
+				setEntity(dx1, dy1, l);
+				setOrientation(l, right);
+			} else { // laser causes explosion
+				setX(dx, dy, l);
+				wizz(l);
+			}
+			if (getEntity(dx2, dy2) == null) { // one half "moved" on the left
+				setEntity(dx2, dy2, l_copie);
+				setOrientation(l_copie, left);
+			} else { // laser causes explosion
+				setX(dx, dy, l_copie);
+				wizz(l_copie);
+			}
+
 		}
 
 	}
-	
-	
+
+	public void hit (Entity E) {
+		
+		if (E == null) {  // just in case getEntity gives it null
+			return;
+		}
+		if (getKillable(E) == true ) {  
+			E.vie = E.vie - 1;
+		}
+	}
+
 	public void Wizz (Laser l, Map map) {
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
+		int cx = GetX(l);
+		int cy = GetY(l);
 		
 		// hit all entities on the 8 adjacent tiles
-		hit.(map.getEntity(cx+1, cy));
-		hit.(map.getEntity(cx, cy+1));
-		hit.(map.getEntity(cx+1, cy+1));
-		hit.(map.getEntity(cx, cy-1));
-		hit.(map.getEntity(cx-1, cy-1));
-		hit.(map.getEntity(cx+1, cy-1));
-		hit.(map.getEntity(cx-1, cy+1));
-		hit.(map.getEntity(cx-1, cy+1));
+		hit(getEntity(cx+1, cy)));
+		hit(getEntity(cx, cy+1)));
+		hit(getEntity(cx+1, cy+1)));
+		hit(getEntity(cx, cy-1)));
+		hit(getEntity(cx-1, cy-1)));
+		hit(getEntity(cx+1, cy-1)));
+		hit(getEntity(cx-1, cy)));
+		hit(getEntity(cx-1, cy+1)));
 		
-		Despawn(l);
+		deleteEntity(l);
 	}
 
 	
-	// despawns laser once it's out of time
-	public void Despawn(Laser l) {
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
-		
-		map[cx][cy]= 'n';
-		l = null ;
-			
-	}
-	
-	// movement ( whether or not they're allowed checked by the automaton )
-	
-	public void Move_up (Laser l) {
-		
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
-		
-		map[cx][cy]= 'n';   // we free the current tile
-		map[cx][cy-1]= 'l'; // fill the new location
-		l.lifespan--;       // decrease the time before the laser despawns
-		if( l.lifespan ==0) {  // if out of time, laser dispawns
-			Despawn(l);
+
+	// absolute movement ( whether or not they're allowed checked by the automaton )
+
+	public void move_up(Laser l) {
+
+		int cx = GetX(l);
+		int cy = GetY(l);
+		int dx,dy;
+		int life = getLife(l);
+
+		dx = cx;
+		dy = dy - 1;
+		if (getEntity(dx, dy) == null) {
+			setEntity(dx,dy,l)  // moves laser to the new location
+			life --;
+			if (life == 0) { // if out of time, laser despawns
+				deleteEntity(l);
+			}
+			setLife(l, life) // decrease the time before the laser despawns
+		}
+		else {
+			hit(getEntity(dx,dy));
+			deleteEntity(l);
 		}
 		
+
 	}
-	
-	public void Move_down (Laser l) {
+
+	public void move_down(Laser l) {
 		
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
-		
-		map[cx][cy]= 'n';   // we free the current tile
-		map[cx][cy+1]= 'l'; // fill the new location
-		l.lifespan--;       // decrease the time before the laser despawns
-		if( l.lifespan ==0) { // if out of time, laser dispawns
-			Despawn(l);
+		int cx = GetX(l);
+		int cy = GetY(l);
+		int dx,dy;
+		int life = getLife(l);
+
+		dx = cx;
+		dy = dy +1;
+		if (getEntity(dx, dy) == null) {
+			setEntity(dx,dy,l)  // moves laser to the new location
+			life --;
+			if (life == 0) { // if out of time, laser despawns
+				deleteEntity(l);
+			}
+			setLife(l, life) // decrease the time before the laser despawns
+		}
+		else {
+			hit(getEntity(dx,dy));
+			deleteEntity(l);
 		}
 	}
-	
-	public void Move_right (Laser l) {
-		
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
-		
-		map[cx][cy]= 'n';   // we free the current tile
-		map[cx+1][cy]= 'l'; // fill the new location
-		l.lifespan--;       // decrease the time before the laser despawns
-		if( l.lifespan ==0) { // if out of time, laser dispawns
-			Despawn(l);
+
+	public void move_right(Laser l) {
+
+		int cx = GetX(l);
+		int cy = GetY(l);
+		int dx,dy;
+		int life = getLife(l);
+
+		dx = cx+1;
+		dy = cy;
+		if (getEntity(dx, dy) == null) {
+			setEntity(dx,dy,l)  // moves laser to the new location
+			life --;
+			if (life == 0) { // if out of time, laser despawns
+				deleteEntity(l);
+			}
+			setLife(l, life) // decrease the time before the laser despawns
+		}
+		else {
+			hit(getEntity(dx,dy));
+			deleteEntity(l);
 		}
 	}
-	
-	public void Move_left (Laser l) {
-		
-		int cx = l.coordinateX;
-		int cy = l.coordinateY;
-		
-		map[cx][cy]= 'n';   // we free the current tile
-		map[cx-1][cy]= 'l'; // fill the new location
-		l.lifespan--;       // decrease the time before the laser despawns
-		if( l.lifespan == 0) { // if out of time, laser dispawns
-			Despawn(l);
+
+	public void move_left(Laser l) {
+
+		int cx = GetX(l);
+		int cy = GetY(l);
+		int dx,dy;
+		int life = getLife(l);
+
+		dx = cx;
+		dy = dy - 1;
+		if (getEntity(dx, dy) == null) {
+			setEntity(dx,dy,l)  // moves laser to the new location
+			life --;
+			if (life == 0) { // if out of time, laser despawns
+				deleteEntity(l);
+			}
+			setLife(l, life) // decrease the time before the laser despawns
+		}
+		else {
+			hit(getEntity(dx,dy));
+			deleteEntity(l);
 		}
 	}
 
 	
 	
+	// relative movement (uses the laser's orientation)
+	
+	public void move_up_relative(Laser l) {
+		
+		Orientation o = getOrientation(l);
+		
+		switch (o) {
+			case up : move_up(l);
+			case down : move_down(l);
+			case left : move_left(l);
+			case right : move_right(l);	
+		}
+		
+	}
+	
+	public void move_down_relative(Laser l) {
+		
+		Orientation o = getOrientation(l);
+		
+		switch (o) {
+			case up : move_down(l);
+			case down : move_up(l);
+			case left : move_right(l);
+			case right : move_left(l);	
+		}
+		
+	}
+	
+	public void move_right_relative(Laser l) {
+		
+		Orientation o = getOrientation(l);
+		
+		switch (o) {
+			case up : move_right(l);
+			case down : move_left(l);
+			case left : move_up(l);
+			case right : move_down(l);	
+		}
+		
+	}
+	
+	public void move_left_relative(Laser l) {
+		
+		Orientation o = getOrientation(l);
+		
+		switch (o) {
+			case up : move_left(l);
+			case down : move_right(l);
+			case left : move_down(l);
+			case right : move_up(l);	
+		}
+		
+	}
 	
 	
 	
@@ -156,11 +311,11 @@ public class Laser extends Being {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
+	// TO DO
+	/*
+	 * 
+	 * 
+	 * 
+	 */
+
 }
