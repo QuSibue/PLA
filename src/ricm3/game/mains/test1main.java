082f5b7c28b26af90fc1ed2c55f0f70086118949
Nicolas Castano <castanonicolas.nc@gmail.com>
2018-06-04 18:29:14 +0200

    tmp

---
+++ src/ricm3/game/mains/test1main.java
@@ -0,0 +1,13 @@
+package ricm3.game.mains;
+
+import ricm3.game.mvc.Model;
+
+public class test1main {
+
+	public static void main(String[] args) {
+		Model model = new Model();
+		model.step(0);
+		model.step(0);
+		model.step(0);
+	}
+}
