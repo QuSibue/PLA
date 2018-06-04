5d80b2ef554e068c6c79acfee50a10db99c92de5
Juliette LAPLANTE <juliette.laplante.jl@gmail.com>
2018-06-04 10:44:17 +0200

    1ère implementation map avec sauvergarde

--- src/ricm3/game/mvc/MainMap.java
+++ src/ricm3/game/mvc/MainMap.java
@@ -1,24 +1,10 @@
 package ricm3.game.mvc;
 
 public class MainMap {
-	
+
 	public static void main(String[] args) {
 		Model m = new Model();
-		int i = 0;
-		int j = 0;
-		for (i = 0; i < m.carte.height; i++) {
-			for (j = 0; j < m.carte.length; j++) {
-				if(m.carte.matrice[i][j] != null) {
-				(m.carte.matrice[i][j]).pretty_print();
-				}
-				else {
-					System.out.print(m.carte.matrice[i][j]);
-				}
-					
-				System.out.print(" ");
-			}	
-			System.out.println();
-		}
+		m.carte.printMap();
 	}
 
 }
--- src/ricm3/game/mvc/Map.java
+++ src/ricm3/game/mvc/Map.java
@@ -1,7 +1,6 @@
 package ricm3.game.mvc;
 
 import ricm3.game.entity.Entity;
-import ricm3.game.entity.Wall;
 
 //import java.io.File;
 //import java.io.FileInputStream;
@@ -23,33 +22,47 @@ public class Map {
 			}
 		}
 	}
-	
-	
+
 	public boolean setEntity(Entity E) {
-		matrice[E.getX()][E.getY()]=E;
+		matrice[E.getX()][E.getY()] = E;
 		return true;
 	}
-	
 
 	public boolean setEntity(int x, int y, Entity E) {
-		E.setX(x); 
+		E.setX(x);
 		E.setY(y);
 		setEntity(E);
 		return true;
 	}
 
-
 	public Entity getEntity(int x, int y) {
 		return matrice[x][y];
 	}
-	
+
 	public void deleteEntity(Entity E) {
-		matrice[E.getX()][E.getY()]=null;
+		matrice[E.getX()][E.getY()] = null;
 	}
-	
+
 	public void moveEntity(Entity E, int x, int y) {
 		deleteEntity(E);
-		setEntity(x,y,E);
+		setEntity(x, y, E);
+	}
+
+	public void printMap() {
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < height; i++) {
+			for (j = 0; j < length; j++) {
+				if (matrice[i][j] != null) {
+					(matrice[i][j]).pretty_print();
+				} else {
+					System.out.print(matrice[i][j]);
+				}
+
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
